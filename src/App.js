4b2c9f751c5569ddd8dea5d49ecf26aa9abf2547
LuanMed <luanmede@gmail.com>
2023-01-25 11:12:27 -0300

    test/ trocar de branch e dar merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path='/home' element={<HomePage />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/HomePage.js
@@ -0,0 +1,59 @@
+import logo from "../assets/images/Organic Store - fundo transparente.png";
+import styled from "styled-components";
+import { AiFillHome } from 'react-icons/ai';
+
+export default function HomePage() {
+    return (
+        <ContainerHome>
+            <ContainerLogo>
+                <img src={logo} />
+            </ContainerLogo>
+            <ContainerMain>
+                <h1>Bem vindo(a) Fulano</h1>
+                <h2>ESCOLHA OS PRODUTOS ABAIXO</h2>
+            </ContainerMain>
+            <ContainerMenu>
+                <AiFillHome color="#FFFFFF"/>
+            </ContainerMenu>
+        </ContainerHome>
+    );
+}
+
+const ContainerHome = styled.div`
+    
+`
+
+const ContainerLogo = styled.header`
+    display: flex;
+    justify-content: center;
+    background-color: #6EB122;
+    img{
+        width: 150px;
+    }
+`
+
+const ContainerMain = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 15px;
+    h1{
+        font-size: 20px;
+        font-weight: 700;
+        color: #008037;
+        margin-bottom: 10px;
+    }
+    h2{
+        color: #6EB122;
+    }
+`
+
+const ContainerMenu = styled.footer`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100vw;
+    height: 50px;
+    background-color: #6EB122;
+    z-index: 1;
+`
