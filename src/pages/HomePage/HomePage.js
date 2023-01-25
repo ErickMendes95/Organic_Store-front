13f3390f62ca62a6d762e556cf121ad0ad7578ed
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-01-25 14:31:01 -0300

    feat: Adiciona Layout estático na página Home

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,21 @@
-import styled from "styled-components"
+import {Container, StyledButton} from './style'
+import LogoInicio from "../../assets/images/LogoInicio.png"
 
-export default function HomePage(){
-    return
+export default function HomePage() {
+
+  return (
+    <Container>
+      <img src={LogoInicio} alt="OrganicStore"/>
+
+
+        <StyledButton type="submit">
+          Cadastrar
+        </StyledButton>
+
+        <StyledButton type="submit">
+          Já tenho uma conta
+        </StyledButton>
+
+    </Container>
+  )
 }
